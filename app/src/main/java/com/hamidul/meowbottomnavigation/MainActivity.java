edc51854e922a0f3246c9887df7398e5490d7c87
user <practicecoddin@gmail.com>
2023-09-12 20:14:57 +0600

    Meow Bottom Navigation

--- app/src/main/java/com/hamidul/meowbottomnavigation/MainActivity.java
+++ app/src/main/java/com/hamidul/meowbottomnavigation/MainActivity.java
@@ -4,11 +4,102 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MeowBottomNavigation bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigation = findViewById(R.id.bottomNavigation);
+
+
+        bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.baseline_settings_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.baseline_home_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3,R.drawable.baseline_person_24));
+
+
+        bottomNavigation.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+
+                switch (model.getId()){
+
+                    case 1:
+
+
+                        break;
+
+                    case 2:
+
+
+                        break;
+
+                    case 3:
+
+
+                        break;
+
+                }
+                return null;
+            }
+        });
+
+        bottomNavigation.setOnShowListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+
+                switch (model.getId()){
+
+                    case 1:
+
+
+                        break;
+                }
+
+                return null;
+            }
+        });
+
+        bottomNavigation.setOnShowListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+
+                switch (model.getId()){
+
+                    case 2:
+
+
+                        break;
+                }
+
+                return null;
+            }
+        });
+
+        bottomNavigation.setOnShowListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+
+                switch (model.getId()){
+
+                    case 3:
+
+
+                        break;
+                }
+
+                return null;
+            }
+        });
+
+
     }
 }
